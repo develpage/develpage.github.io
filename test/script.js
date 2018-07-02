3a7b09f4261ce1ffb42e978b16fd9bc55d588fb2
Vadim Polyvyanov <v.polyvyanov@cinet.ru>
2018-07-02 16:07:07 +0300

    sd

--- test/script.js
+++ test/script.js
@@ -3,14 +3,33 @@
 document.onload = function (){
 
 var noSleep = new NoSleep();
-
-function enableNoSleep() {
   noSleep.enable();
-  document.removeEventListener('touchstart', enableNoSleep, false);
-}
-
-// Enable wake lock.
-// (must be wrapped in a user input event handler e.g. a mouse or touch handler)
-document.addEventListener('touchstart', enableNoSleep, false);
 
 };
+
+
+
+
+
+
+/*
+
+var noSleep = new NoSleep();
+
+var wakeLockEnabled = false;
+var toggleEl = document.querySelector("#toggle");
+toggleEl.addEventListener('click', function() {
+  if (!wakeLockEnabled) {
+    noSleep.enable(); // keep the screen on!
+    wakeLockEnabled = true;
+    toggleEl.value = "Wake Lock is enabled";
+    document.body.style.backgroundColor = "green";
+  } else {
+    noSleep.disable(); // let the screen turn off.
+    wakeLockEnabled = false;
+    toggleEl.value = "Wake Lock is disabled";
+    document.body.style.backgroundColor = "";
+  }
+}, false);
+
+*/
