a8f88b6af6f348180c13722ebbc0355c65a1425c
Vadim Polyvyanov <v.polyvyanov@cinet.ru>
2018-07-02 16:18:47 +0300

    sd

--- test/script.js
+++ test/script.js
@@ -1,24 +1,21 @@
 
 
-document.onload = function (){
 
 var noSleep = new NoSleep();
 var wakeLockEnabled = false;
-var toggleEl = document.querySelector("body");
 
-toggleEl.addEventListener('click', function() {
+
+document.addEventListener('click', function() {
+  console.log('eee');
   if (!wakeLockEnabled) {
     noSleep.enable(); // keep the screen on!
     wakeLockEnabled = true;
-    toggleEl.value = "Wake Lock is enabled";
-    document.body.style.backgroundColor = "green";
   } else {
    
   }
 }, false);
 
 
-};
 
 
 /*
